efae099f13e00bb522d51217a8e9242d01d736c1
mengmeng <lulumia@qq.com>
2017-08-03 18:22:30 +0800

    use compass to compile

---
+++ sass_demo/config.rb
@@ -0,0 +1,11 @@
+http_path = "/"
+css_dir = "css"
+sass_dir = "sass"
+scss_dir = "sass"
+images_dir = "images"
+javascripts_dir = "js"
+fonts_dir = "fonts"
+
+output_style = :expanded
+environment = :development
+relative_assets = true
